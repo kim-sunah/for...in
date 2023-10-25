f6c137c8425f162daddca9c03d2132ced59b77f1
sunah <1004_sunah@naver.com>
2023-10-25 20:30:23 +0900

    동영상 &포스터 끝!

--- js/detail.js
+++ js/detail.js
@@ -70,7 +70,6 @@ function detailpage(response) {
         <img class="poster" src="https://www.themoviedb.org/t/p/w220_and_h330_face${response.poster_path}" srcset="https://www.themoviedb.org/t/p/w220_and_h330_face${response.poster_path} 1x, https://www.themoviedb.org/t/p/w440_and_h660_face${response.poster_path} 2x" alt="늙은 아빠들"></div>
     </div>
         `)
-
     $('.image_content').html(`
     <img class="poster lazyload lazyloaded"
     src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2${response.poster_path}"
@@ -159,14 +158,25 @@ $('.menu li').click(function (e) {
 
     // liId에 따라 다른 정보를 표시
     if (liId === 'popular') {
-        movieImages.html('<p>인기 있는 정보를 표시합니다.</p>');
+        fetch(`https://api.themoviedb.org/3/movie/${url}?append_to_response=credits&language=ko-kr`, options)
+            .then(response => response.json())
+            .then(response =>
+                $('#media_scroller').html(`
+        <div class="h_scroller content scroller" id = "movie_images">
+            <div class="backdrop">
+                <img loading="lazy" class="backdrop"src="https://www.themoviedb.org/t/p/w533_and_h300_bestv2${response.backdrop_path}"srcset="https://www.themoviedb.org/t/p/w533_and_h300_bestv2${response.backdrop_path} 1x, https://www.themoviedb.org/t/p/w1066_and_h600_bestv2${response.backdrop_path} 2x"alt="${response.name}">
+            </div>
+            <div class="poster">
+            <img class="poster" src="https://www.themoviedb.org/t/p/w220_and_h330_face${response.poster_path}" srcset="https://www.themoviedb.org/t/p/w220_and_h330_face${response.poster_path} 1x, https://www.themoviedb.org/t/p/w440_and_h660_face${response.poster_path} 2x" alt="늙은 아빠들"></div>
+        </div>
+            `)
+            ).catch(err => console.error(err));
     } else if (liId === 'videos') {
         $('#media_scroller').html(` <div class="h_scroller content scroller" id = "video_list"></div>`)
         fetch(`https://api.themoviedb.org/3/movie/${url}/videos?language=en-US`, options)
             .then(response => response.json())
             .then(response =>
                 response.results.forEach(e => {
-                    console.log(e)
                     $('#video_list').append(`
                         <div class="video card no_border">
                             <div class="wrapper" style="background-image: url('https://i.ytimg.com/vi/${e.key}/hqdefault.jpg');">
@@ -177,98 +187,57 @@ $('.menu li').click(function (e) {
                 })
             )
             .catch(err => console.error(err));
-        // 클릭한 a 요소에 대한 클릭 이벤트 리스너 추가
         $(document).on('click', '.play_trailer', function (e) {
-            e.preventDefault(); // 링크의 기본 동작 방지
-            // 해당 동영상의 데이터 가져오기
-            const site = $(this).data('site');
+            e.preventDefault();
             const id = $(this).data('id');
             const title = $(this).data('title');
-
-            // YouTube 동영상 재생을 위한 URL 생성
-            const videoUrl = `https://www.youtube.com/watch?v=${id}`;
-
-            // movie 객체 생성
-            const movie = {
-                site: site,
-                id: id,
-                title: title,
-                url: videoUrl
-            };
-
-            // movieClick 함수 호출
-            movieClick(movie);
+            $('body').append(`
+            <div class="k-overlay" style="display: block; z-index: 10003; opacity: 0.5;"></div>
+            <div class="k-widget k-window k-display-inline-flex k-state-focused" tabindex="0" style="min-width: 90px; min-height: 50px; width: 789px; height: 506px; display: none; top: 7.5px; left: 143.5px; background-color: rgb(0, 0, 0); border-color: rgb(0, 0, 0); position: fixed; z-index: 10004; opacity: 1;"><div class="k-window-titlebar" style="background-color: rgb(0, 0, 0); border-color: rgb(0, 0, 0); color: rgb(255, 255, 255);">
+            <span class="k-window-title" id="video_popup_wnd_title">${title}</span><div class="k-window-actions"><a role="button" href="#" class="k-button k-flat k-button-icon k-window-action" aria-label="Close"><span class="k-icon k-i-close"></span></a></div></div><div id="video_popup" data-role="window" class="k-window-content" role="dialog" aria-labelledby="video_popup_wnd_title" style="display: block; padding: 0px; margin: 0px; background-color: rgb(0, 0, 0);">
+            <title>Play NjbizH7cx5k on YouTube</title>
+            <meta charset="utf-8">
+            <style>
+            body.trailer_iframe { background-color: #000; padding: 0; margin: 0; overflow: hidden; }
+            iframe { background-color: #000; overflow: hidden; }
+            </style>
+            <iframe type="text/html" style="background-color: #000;" width="789" height="443" src="//www.youtube.com/embed/${id}?autoplay=1&amp;origin=https%3A%2F%2Fwww.themoviedb.org&amp;hl=ko&amp;modestbranding=1&amp;fs=1&amp;autohide=1" frameborder="0" allowfullscreen=""></iframe>
+            </div></div>
+            `)
+            $('a[aria-label="Close"]').click(function (e) {
+                e.preventDefault();
+                $('.k-overlay').remove();
+                $('.k-widget').remove();
+            });
         });
 
-        // YouTube 동영상을 팝업으로 표시하는 함수
-        function movieClick(movie) {
-            console.log(movie);
-
-            // 팝업 열기
-            openVideoPopup(movie.title, movie.url);
-        }
-
-        // 팝업을 열기 위한 함수
-        function openVideoPopup(title, videoUrl) {
-            // YouTube 동영상을 표시할 iframe을 생성
-            console.log(videoUrl)
-            const iframeCode = `
-        <iframe width="560" height="315" src="${videoUrl}" frameborder="0" allowfullscreen></iframe>
-    `;
-
-            // 팝업 열기
-            $('body').append(`
-        <div class="video-popup">
-            <h2>${title}</h2>
-            ${iframeCode}
-        </div>
-    `);
-        }
-
-
     } else if (liId === 'backdrops') {
-        movieImages.html('<p>배경 정보를 표시합니다.</p>');
+        $('#media_scroller').html(` <div class="h_scroller content scroller" id = "video_list"></div>`)
+        fetch(`https://api.themoviedb.org/3/movie/${url}/images`, options)
+            .then(response => response.json())
+            .then(response => response.backdrops.forEach(e => {
+                    $('#video_list').append(`
+                    <div class="backdrop">
+                    <img loading="lazy" class="backdrop" src="https://www.themoviedb.org//t/p/w533_and_h300_bestv2${e.file_path}" srcset="https://www.themoviedb.org//t/p/w533_and_h300_bestv2${e.file_path} 1x, https://www.themoviedb.org/t/p/w1066_and_h600_bestv2${e.file_path} 2x" alt="쏘우 10">
+                    </div>
+                    `)
+            })
+            ).catch(err => console.error(err));
     } else if (liId === 'posters') {
-        movieImages.html('<p>포스터 정보를 표시합니다.</p>');
+        $('#media_scroller').html(` <div class="h_scroller content scroller" id = "video_list"></div>`)
+        fetch(`https://api.themoviedb.org/3/movie/${url}/images`, options)
+            .then(response => response.json())
+            .then(response =>
+                response.posters.forEach(e => {
+                    $('#video_list').append(`
+                    <div class="backdrop">
+                    <img loading="lazy" class="backdrop" src="https://www.themoviedb.org//t/p/w533_and_h300_bestv2${e.file_path}" srcset="https://www.themoviedb.org//t/p/w533_and_h300_bestv2${e.file_path} 1x, https://www.themoviedb.org/t/p/w1066_and_h600_bestv2${e.file_path} 2x" alt="쏘우 10">
+                    </div>
+                    `)
+            })
+            ).catch(err => console.error(err));
     }
 });
 
-
-
-function movieClick(movie) {
-    console.log(movie)
-    $('body').append(`
-    <div class="k-overlay" style="display: block; z-index: 10003; opacity: 0.5;"></div>
-    <div class="k-widget k-window k-state-focused k-display-inline-flex" tabindex="0" style="min-width: 90px; min-height: 50px; width: 789px; height: 506px; display: none; top: 7.5px; left: 443.5px; background-color: rgb(0, 0, 0); border-color: rgb(0, 0, 0); position: fixed; z-index: 10004; opacity: 1;">
-    <div class="k-window-titlebar" style="background-color: rgb(0, 0, 0); border-color: rgb(0, 0, 0); color: rgb(255, 255, 255);">
-    <span class="k-window-title" id="video_popup_wnd_title">[반지의 제왕: 왕의 귀환 (The Lord Of The Rings: The Return Of The King)]예고편</span>
-    <div class="k-window-actions">
-        <a role="button" href="#" class="k-button k-button-icon k-window-action k-flat"aria-label="Close">
-            <span class="k-icon k-i-close"></span>
-        </a>
-    </div>
-  </div>
-  <div id="video_popup" data-role="window" class="k-window-content" role="dialog" aria-labelledby="video_popup_wnd_title" style="padding: 0px; margin: 0px; background-color: rgb(0, 0, 0); visibility: visible; display: block;">
-    <title>Play 3EEGmuB7ADo on YouTube</title>
-    <meta charset="utf-8">
-    <style>
-      body.trailer_iframe {
-        background-color: #000;
-        padding: 0;
-        margin: 0;
-        overflow: hidden;
-      }
-      iframe {
-        background-color: #000;
-        overflow: hidden;
-      }
-    </style>
-    <iframe type="text/html" style="background-color: #000;" width="789" height="443"
-      src="//www.youtube.com/embed/3EEGmuB7ADo?autoplay=1&amp;origin=https%3A%2F%2Fwww.themoviedb.org&amp;hl=ko&amp;modestbranding=1&amp;fs=1&amp;autohide=1"
-      frameborder="0" allowfullscreen="">
-    </iframe>
-  </div>
-</div>`)
-}
 
 
