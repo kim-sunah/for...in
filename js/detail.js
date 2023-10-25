21a070e42a63cd6ed0af714263a34e3ff8d17e3c
sunah <1004_sunah@naver.com>
2023-10-25 10:36:55 +0900

    review

--- js/detail.js
+++ js/detail.js
@@ -1,3 +1,4 @@
+const url = new URL(document.location.href).searchParams.get('id');
 const options = {
     method: 'GET',
     headers: {
@@ -5,43 +6,63 @@ const options = {
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMzliNjIwOTFmMzY0Y2M4MzczMGExMzU3ZWM1YjE3ZCIsInN1YiI6IjY1MmY3NTcyMzU4ZGE3NWI1ZDAwODcyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.j6JDptMTCwZT8Gkr2PbQ2rWV5r85H1fKNwS4iF1_o3U'
     }
 };
-fetch('https://api.themoviedb.org/3/movie/238?append_to_response=credits&language=ko-kr', options)
+fetch(`https://api.themoviedb.org/3/movie/${url}?append_to_response=credits&language=ko-kr`, options)
     .then(response => response.json())
     .then(response =>
+        // console.log(response)
         detailpage(response)
     )
     .catch(err => console.error(err));
-function review() {
-    location.href('review.html')
-}
-
+    
 function detailpage(response) {
     console.log(response)
-    $(".txt_tit").text(response.title)
-    $(".movie_image").html(`
-        <img class="bg_img" src="https://image.tmdb.org/t/p/w500/${response.poster_path}"
-                        alt="">
-    `)
-    $(".txt_name").text(`${response.original_title}, ${response.release_date}`)
-    $(".release_date").text(response.release_date)
-    $(".vote_average").text(response.vote_average)
+    $("#title").text(response.title)
+    $("#year").text(`(${response.release_date.slice(0, 4)})`)
+    $("#release_date").text(`${response.release_date}(${response.production_countries[0].iso_3166_1})`)
     response.genres.forEach(element => {
-        $(".genres").append(`${element.name}/`)
+        $("#genres").append(`${element.name}, `)
     });
-    $(".genres").text($(".genres").text().slice(0, -1))
-    $(".production_countries").text(response.production_countries[0].name)
-    $('.runtime').text(`${response.runtime} 분`)
-    $('.contents').html(`
-    <h4 class="screen_out">주요정보</h4>
-        <div class="detail_basicinfo">
-            <div class="movie_summary">
-                <div class="info_desc" data-tiara-layer="desc">
-                    <div class="desc_cont expand" id="overview">
-                        ${response.overview.replace(/\. /g, '.<br/>')}
-                    </div>
-                </div>
-            </div>
+    $("#genres").text($("#genres").text().slice(0, -2))
+    $(".runtime").text(`${Math.floor(response.runtime / 60)}h ${response.runtime % 60}m`)
+    $(".overview").text(response.overview)
+    for (var i = 0; i < 6; i++) {
+        $("#credits").append(`
+        <li class="card">
+            <a>
+                <img loading="lazy" class="profile" src="https://www.themoviedb.org/t/p/w276_and_h350_face${response.credits.cast[i].profile_path}" 
+                srcset="https://www.themoviedb.org/t/p/w276_and_h350_face${response.credits.cast[i].profile_path} 1x, https://www.themoviedb.org/t/p/w276_and_h350_face${response.credits.cast[i].profile_path} 2x" alt="${response.credits.cast[i].name}">
+            </a>
+            <p><a>${response.credits.cast[i].name}</a></p>
+            <p class="character">${response.credits.cast[i].character}</p>
+         </li>
+        `)
+        response.credits.cast[i]
+    }
+    $('#movie_images').append(`
+        <div class="backdrop">
+            <img loading="lazy" class="backdrop"src="https://www.themoviedb.org/t/p/w533_and_h300_bestv2${response.backdrop_path}"srcset="https://www.themoviedb.org/t/p/w533_and_h300_bestv2${response.backdrop_path} 1x, https://www.themoviedb.org/t/p/w1066_and_h600_bestv2${response.backdrop_path} 2x"alt="${response.name}">
         </div>
-    <button onclick="location.href = 'review.html?movie_id=${response.id}'">리뷰</button>
+        <div class="poster">
+        <img class="poster" src="https://www.themoviedb.org/t/p/w220_and_h330_face${response.poster_path}" srcset="https://www.themoviedb.org/t/p/w220_and_h330_face${response.poster_path} 1x, https://www.themoviedb.org/t/p/w440_and_h660_face${response.poster_path} 2x" alt="늙은 아빠들"></div>
     `)
+    $('.original_content').append(`
+    <div class="review_container zero">
+        <div class="content zero">
+            <p class="no_margin" dir="auto">
+            ${response.title}에 관한 리뷰가 없습니다. 
+            <a href="review.html?id=${response.id}">리뷰 쓰기</a>
+            </p>
+        </div>
+    </div>
+    `)
+    $('.image_content').append(`
+    <img class="poster lazyload lazyloaded"
+    src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2${response.poster_path}"
+    data-src="https://www.themoviedb.org/t/p/w300_and_h450_bestv2${response.poster_path}"
+    data-srcset="https://www.themoviedb.org/t/p/w300_and_h450_bestv2${response.poster_path} 1x, https://www.themoviedb.org/t/p/w600_and_h900_bestv2${response.poster_path} 2x"
+    alt="${response.title}"
+    srcset="https://www.themoviedb.org/t/p/w300_and_h450_bestv2${response.poster_path} 1x, https://www.themoviedb.org/t/p/w600_and_h900_bestv2${response.poster_path} 2x"
+    data-loaded="true">
+    `)
+    $('#backgroundImage').css('background-image' , `url('https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces${response.backdrop_path}')`)
 }
--- js/review.js
+++ js/review.js
@@ -5,6 +5,8 @@ import { getDocs, getDoc, deleteDoc, addDoc, collection, doc, updateDoc } from "
 import { query, orderBy, where } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 // import { getAnalytics } from "firebase/analytics";
 
+const _REVIEW = "review"
+var reviewArr = [];
 // Firebase 구성 정보 설정
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -20,3 +22,83 @@ const firebaseConfig = {
 // Firebase 인스턴스 초기화
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const url = new URL(document.location.href).searchParams.get('id');
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMzliNjIwOTFmMzY0Y2M4MzczMGExMzU3ZWM1YjE3ZCIsInN1YiI6IjY1MmY3NTcyMzU4ZGE3NWI1ZDAwODcyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.j6JDptMTCwZT8Gkr2PbQ2rWV5r85H1fKNwS4iF1_o3U'
+    }
+};
+fetch(`https://api.themoviedb.org/3/movie/${url}?append_to_response=credits&language=ko-kr`, options)
+    .then(response => response.json())
+    .then(response =>
+        // console.log(response)
+        templet(response)
+    )
+    .catch(err => console.error(err));
+
+
+
+var today = new Date();
+var year = today.getFullYear();
+var month = ('0' + (today.getMonth() + 1)).slice(-2);
+var day = ('0' + today.getDate()).slice(-2);
+var hours = ('0' + today.getHours()).slice(-2);
+var minutes = ('0' + today.getMinutes()).slice(-2);
+var seconds = ('0' + today.getSeconds()).slice(-2);
+
+var dateString = year + '-' + month + '-' + day + " " + hours + ':' + minutes + ':' + seconds;;
+
+function templet(response) {
+    console.log(response)
+    $('.col1').append(`
+        <a href="/detail?id=${response.id}">
+            <img class="poster" src="https://www.themoviedb.org/t/p/w220_and_h330_face${response.poster_path}" srcset="https://www.themoviedb.org/t/p/w220_and_h330_face${response.poster_path} 1x, https://www.themoviedb.org/t/p/w440_and_h660_face${response.poster_path} 2x" alt="쏘우 10">
+        </a>
+    `)
+    $('#text').append(`
+        <h2 class="space">A review by 유저이름</h2>
+        <h3>Title: <span>${response.title} (${response.release_date.slice(0, 4)})</span></h3>
+        <div id="editor-textarea" class="column">
+            <div class="editor">
+                <textarea placeholder="You can start writing your review here."></textarea>
+            </div>
+        </div>
+    `)
+    $('.cancle').attr('href',`detail.html?id=${url}`)
+}
+
+$('#review').on('submit', (enent) => {
+    enent.preventDefault();
+    addComment()
+})
+
+
+async function addComment() {
+    let name = localStorage.getItem('userName')
+    if ($('textarea').val() == "") {
+        alert('댓글이 입력되지 않았습니다.')
+        return
+    } else if (name == undefined) {
+        name = prompt("성함을 알려주세요~");
+        if (name == "")return;
+        else localStorage.setItem('userName', name);
+    }
+    let password = prompt("게시글의 비밀번호를 입력해주세요");
+    if (password) {
+        let doc = {
+            'id': today,
+            'userName': localStorage.getItem('userName'),
+            'comment': $('textarea').val(),
+            'password': password,
+            'date': dateString,
+        }
+        await addDoc(collection(db, "review"), doc);
+        alert('저장 완료!');
+        reviewArr.push(doc)
+        localStorage.setItem(_REVIEW, JSON.stringify(reviewArr));
+        getCommentList()
+    }
+}
+// $('.cancle').on('click',location.href(`detail.html?id=${url}`))
