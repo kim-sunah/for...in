afe4474c48f6d618f447e5293f48974a48edd076
wooseob <dntjq5130@naver.com>
2023-10-25 09:39:33 +0900

    1차완성

--- 태그/modal.js
+++ 태그/modal.js
@@ -1,6 +1,7 @@
-const showModalBtn = document.getElementById("applyFilterBtn")
 const closeModalBtn = document.getElementById("closeModalBtn")
-const applyFilterBtn = document.getElementById("applyFilterBtn")
+const startBtn = document.getElementById("startBtn")
+const modal = document.getElementById("myModal");
+const genreFilter = document.getElementById("genreFilter")
 
 
 const options = {
@@ -10,60 +11,71 @@ const options = {
         Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMzliNjIwOTFmMzY0Y2M4MzczMGExMzU3ZWM1YjE3ZCIsInN1YiI6IjY1MmY3NTcyMzU4ZGE3NWI1ZDAwODcyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.j6JDptMTCwZT8Gkr2PbQ2rWV5r85H1fKNwS4iF1_o3U",
     },
 };
-genres.forEach((genre_ids) => {
-    const option = document.createElement("option");
-    option.value = genre.id;
-    option.text = genre.name;
-    genreFilter.appendChild(option);
-});
 
-showModalBtn.addEventListener("click", () => {
-    console log("test")
-    modal.style.display = "block";
+startBtn.addEventListener("click", () => {
+    console.log("test")
+    modal.style.display = "block"
 
 });
+
+fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', options)
+    .then(response => response.json())
+    .then(data => {
+
+        const genres = data.genres;
+
+        genres.forEach(genre => {
+            const option = document.createElement("option");
+            option.value = genre.id;
+            option.text = genre.name;
+            genreFilter.appendChild(option);
+        });
+    })
+    .catch(error => console.error(error));
 
 closeModalBtn.addEventListener("click", () => {
     modal.style.display = "none";
 });
 
-/
+
 applyFilterBtn.addEventListener("click", () => {
     search();
+    modal.style.display = "none";
 });
+console.log("hello")
 
 function search(event) {
     if (event != null) {
         event.preventDefault();
     }
-    const searchgenre = option.id;
-    cardList.innerHTML = "";
 
-    fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', options)
+    const searchgenre = genreFilter.value;
+    const cardList = document.getElementById("cardList");
+
+    let movieCards = [];
+
+
+    fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${searchgenre}&language=en`, options)
         .then(response => response.json())
-        .then(response => {
-            response.results
-                .filter((element) => element.genre.includes(searchgenre))
-                .map((element) => {
+        .then(data => {
+            const results = data.results;
 
-                    cardList.innerHTML += `
-                            <div class="movieCard" id="${element.id}" onclick="movieClick(${element.id})">
-                                <img class="movieImg" src="https://image.tmdb.org/t/p/w500/${element.poster_path}"/>
-                                <h3 class="movieTitle">${element.original_title}</h3>
-                                <p class="movieTxt">${element.overview}</p>
-                                <p class="movieAverage">Rating: ${element.vote_average}</p>
-                            </div>`;
-                });
+
+            results.forEach(element => {
+                const movieCard = `
+                    <div class="movieCard" id="${element.id}" onclick="movieClick(${element.id})">
+                        <img class="movieImg" src="https://image.tmdb.org/t/p/w500/${element.poster_path}"/>
+                        <h3 class="movieTitle">${element.original_title}</h3>
+                        <p class="movieTxt">${element.overview}</p>
+                        <p class="movieAverage">Rating: ${element.vote_average}</p>
+                    </div>`;
+
+
+                movieCards.push(movieCard);
+            });
+
+
+            cardList.innerHTML = movieCards.join("");
         })
-        .catch((err) => console.error(err));
+        .catch(err => console.error(err));
 }
-
-
-const searchForm = document.getElementById("search");
-const searchInput = searchForm.querySelector("input");
-const searchBtn = searchForm.querySelector("button");
-
-searchForm.addEventListener("click", search);
-
-
-search();
